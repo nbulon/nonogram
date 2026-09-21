6fd9e30af334e30fa2001265aad1d878c104bcaf
noahbachmann <dev@noahbachmann.ch>
2026-09-21 13:44:30 +0200

    fix(db): remove unused AppCheckSetup.kt

--- androidApp/src/dev/kotlin/com/trainpaths/nonogram/AppCheckSetup.kt
+++
@@ -1,9 +0,0 @@
-package com.trainpaths.nonogram
-
-import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
-
-fun installAppCheck() {
-    FirebaseAppCheck.getInstance()
-        .installAppCheckProviderFactory(DebugAppCheckProviderFactory.getInstance())
-}
--- androidApp/src/prod/kotlin/com/trainpaths/nonogram/AppCheckSetup.kt
+++
@@ -1,9 +0,0 @@
-package com.trainpaths.nonogram
-
-import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
-
-fun installAppCheck() {
-    FirebaseAppCheck.getInstance()
-        .installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance())
-}
