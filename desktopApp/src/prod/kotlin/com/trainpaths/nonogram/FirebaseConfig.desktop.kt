7d4deb06b2396aa8b6421a141e9af0a13b2fd18e
noahbachmann <dev@noahbachmann.ch>
2026-09-23 15:52:23 +0200

    fix(desk): add desktop api-key for prod

--- desktopApp/src/prod/kotlin/com/trainpaths/nonogram/FirebaseConfig.desktop.kt
+++ desktopApp/src/prod/kotlin/com/trainpaths/nonogram/FirebaseConfig.desktop.kt
@@ -2,7 +2,7 @@ package com.trainpaths.nonogram
 
 // Prod Firebase project
 object FirebaseDesktopConfig {
-    const val API_KEY = "AIzaSyBxhSaP_S1ojV98sv0E2yQbi9f1bZmgKLk"
+    const val API_KEY = "AIzaSyDWKOjt2OudB4YbyyV-4PnVdILDYeAbeUA"
     const val PROJECT_ID = "nonogram-trainpaths"
     const val APP_ID = "1:482051008739:web:538968bebfaa8781533dd6"
     const val GOOGLE_WEB_CLIENT_ID = "482051008739-hc89i851oq6l369ihfd0khfr9g9i46p9.apps.googleusercontent.com"
