d6086b889ab3ee236ecc0b0b09d1bc74ea18e1a2
noahbachmann <dev@noahbachmann.ch>
2026-09-24 10:03:10 +0200

    fix(desk): use diff ip for login

--- shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppInitializer.kt
+++ shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppInitializer.kt
@@ -6,7 +6,7 @@ import com.trainpaths.nonogram.auth.AuthRepository
 import kotlinx.coroutines.CancellationException
 
 object AppInitializer {
-    const val OAUTH_REDIRECT_URI = "http://localhost:56789/callback"
+    const val OAUTH_REDIRECT_URI = "http://localhost:42594/callback"
 
     fun onApplicationStart(googleWebClientId: String) {
         KMPAuth.initialize {
